4cef6946ae3b4c1729bc8ddfbf17e62ecb092f4d
anace09 <anaceworks@gmail.com>
2026-03-04 14:57:57 +0500

    + tests
    categories and comments

---
+++ WebApi.PostsService.Tests/CategoriesControllerTests.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.PostsService.Controllers;
+using WebApi.PostsService.Data;
+using WebApi.PostsService.Models;
+using Xunit;
+
+namespace WebApi.PostsService.Tests
+{
+    public class CategoriesControllerTests : IDisposable
+    {
+        private readonly AppDbContext _db;
+        private readonly CategoriesController _controller;
+
+        public CategoriesControllerTests()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            _db = new AppDbContext(options);
+            _controller = new CategoriesController(_db);
+        }
+
+        // --- GetAll ---
+
+        [Fact]
+        public async Task GetAll_EmptyDb_ReturnsEmptyList()
+        {
+            var result = await _controller.GetAll();
+            var ok = Assert.IsType<OkObjectResult>(result);
+            var categories = Assert.IsAssignableFrom<IEnumerable<Category>>(ok.Value);
+            Assert.Empty(categories);
+        }
+
+        [Fact]
+        public async Task GetAll_WithCategories_ReturnsAll()
+        {
+            _db.Categories.AddRange(
+                new Category { Name = "Tech" },
+                new Category { Name = "Science" }
+            );
+            await _db.SaveChangesAsync();
+
+            var result = await _controller.GetAll();
+            var ok = Assert.IsType<OkObjectResult>(result);
+            var categories = Assert.IsAssignableFrom<IEnumerable<Category>>(ok.Value);
+            Assert.Equal(2, categories.Count());
+        }
+
+        // --- GetById ---
+
+        [Fact]
+        public async Task GetById_ExistingCategory_ReturnsCategory()
+        {
+            var category = new Category { Name = "Tech" };
+            _db.Categories.Add(category);
+            await _db.SaveChangesAsync();
+
+            var result = await _controller.GetById(category.Id);
+            var ok = Assert.IsType<OkObjectResult>(result);
+            var returned = Assert.IsType<Category>(ok.Value);
+            Assert.Equal(category.Id, returned.Id);
+        }
+
+        [Fact]
+        public async Task GetById_NonExistingCategory_ReturnsNotFound()
+        {
+            var result = await _controller.GetById(999);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        // --- Create ---
+
+        [Fact]
+        public async Task Create_ValidName_ReturnsOk()
+        {
+            var result = await _controller.Create("Tech");
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Create_AddsToDb()
+        {
+            await _controller.Create("Tech");
+            Assert.Equal(1, await _db.Categories.CountAsync());
+        }
+
+        [Fact]
+        public async Task Create_ReturnsCreatedCategory()
+        {
+            var result = await _controller.Create("Tech");
+            var ok = Assert.IsType<OkObjectResult>(result);
+            var category = Assert.IsType<Category>(ok.Value);
+            Assert.Equal("Tech", category.Name);
+        }
+
+        // --- Delete ---
+
+        [Fact]
+        public async Task Delete_ExistingCategory_ReturnsNoContent()
+        {
+            var category = new Category { Name = "Tech" };
+            _db.Categories.Add(category);
+            await _db.SaveChangesAsync();
+
+            var result = await _controller.Delete(category.Id);
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public async Task Delete_NonExistingCategory_ReturnsNotFound()
+        {
+            var result = await _controller.Delete(999);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task Delete_RemovesFromDb()
+        {
+            var category = new Category { Name = "Tech" };
+            _db.Categories.Add(category);
+            await _db.SaveChangesAsync();
+
+            await _controller.Delete(category.Id);
+            Assert.Equal(0, await _db.Categories.CountAsync());
+        }
+
+        public void Dispose() => _db.Dispose();
+    }
+}
---
+++ WebApi.PostsService.Tests/CommentsControllerTests.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.PostsService.Controllers;
+using WebApi.PostsService.Data;
+using WebApi.PostsService.DTOs;
+using WebApi.PostsService.Models;
+
+namespace WebApi.PostsService.Tests
+{
+    public class CommentsControllerTests : IDisposable
+    {
+
+        private readonly AppDbContext _db;
+        private readonly CommentsController _controller;
+        private const string TestUserId = "user-123";
+        private const int TestPostId = 1;
+
+        public CommentsControllerTests()
+        {
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            _db = new AppDbContext(options);
+            _controller = new CommentsController(_db);
+
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(
+                        [ 
+                        new Claim(ClaimTypes.NameIdentifier, TestUserId)
+                        ], "test"))
+                }
+            };
+
+            _db.Posts.Add(new Post { Id = TestPostId, Title = "Test", Content = "Test", AuthorId = TestUserId });
+            _db.SaveChanges();
+
+        }
+
+        // --- GetAll ---
+
+        [Fact]
+        public async Task GetAll_NoComments_ReturnsEmptyList() 
+        {
+
+            var result = await _controller.GetAll(TestPostId);
+
+            var ok = Assert.IsType<OkObjectResult>(result);
+            var comments = Assert.IsAssignableFrom<IEnumerable<Comment>>(ok.Value);
+
+            Assert.Empty(comments);
+
+        }
+
+        [Fact]
+        public async Task GetAll_WithComments_ReturnsAll() 
+        {
+
+            _db.Comments.AddRange(
+                new Comment { Content = "Comment 1", AuthorId = TestUserId, PostId = TestPostId },
+                new Comment { Content = "Comment 2", AuthorId = TestUserId, PostId = TestPostId }
+            );
+            await _db.SaveChangesAsync();
+
+            var result = await _controller.GetAll(TestPostId);
+            var ok = Assert.IsType<OkObjectResult>(result);
+            var comments = Assert.IsAssignableFrom<IEnumerable<Comment>>(ok.Value);
+            Assert.Equal(2, comments.Count());
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsOnlyCommentsForPost()
+        {
+            // Другой пост
+            _db.Posts.Add(new Post { Id = 2, Title = "Other", Content = "Other", AuthorId = TestUserId });
+            _db.Comments.AddRange(
+                new Comment { Content = "Comment 1", AuthorId = TestUserId, PostId = TestPostId },
+                new Comment { Content = "Comment 2", AuthorId = TestUserId, PostId = 2 }
+            );
+            await _db.SaveChangesAsync();
+
+            var result = await _controller.GetAll(TestPostId);
+            var ok = Assert.IsType<OkObjectResult>(result);
+            var comments = Assert.IsAssignableFrom<IEnumerable<Comment>>(ok.Value);
+            Assert.Single(comments);
+        }
+
+        // --- Create ---
+
+        [Fact]
+        public async Task Create_ValidRequest_ReturnsOk()
+        {
+            var result = await _controller.Create(TestPostId, new CreateCommentDto("Hello!"));
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Create_NonExistingPost_ReturnsNotFound()
+        {
+            var result = await _controller.Create(999, new CreateCommentDto("Hello!"));
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task Create_SetsAuthorIdFromToken()
+        {
+            await _controller.Create(TestPostId, new CreateCommentDto("Hello!"));
+            var comment = await _db.Comments.FirstOrDefaultAsync();
+            Assert.Equal(TestUserId, comment!.AuthorId);
+        }
+
+        [Fact]
+        public async Task Create_AddsToDb()
+        {
+            await _controller.Create(TestPostId, new CreateCommentDto("Hello!"));
+            Assert.Equal(1, await _db.Comments.CountAsync());
+        }
+
+        // --- Delete ---
+
+        [Fact]
+        public async Task Delete_OwnComment_ReturnsOk()
+        {
+            var comment = new Comment { Content = "Hello", AuthorId = TestUserId, PostId = TestPostId };
+            _db.Comments.Add(comment);
+            await _db.SaveChangesAsync();
+
+            var result = await _controller.Delete(TestPostId, comment.Id);
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Delete_NonExistingComment_ReturnsNotFound()
+        {
+            var result = await _controller.Delete(TestPostId, 999);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task Delete_OtherUserComment_ReturnsForbid()
+        {
+            var comment = new Comment { Content = "Hello", AuthorId = "other-user", PostId = TestPostId };
+            _db.Comments.Add(comment);
+            await _db.SaveChangesAsync();
+
+            var result = await _controller.Delete(TestPostId, comment.Id);
+            Assert.IsType<ForbidResult>(result);
+        }
+
+        [Fact]
+        public async Task Delete_RemovesFromDb()
+        {
+            var comment = new Comment { Content = "Hello", AuthorId = TestUserId, PostId = TestPostId };
+            _db.Comments.Add(comment);
+            await _db.SaveChangesAsync();
+
+            await _controller.Delete(TestPostId, comment.Id);
+            Assert.Equal(0, await _db.Comments.CountAsync());
+        }
+
+        public void Dispose() => _db.Dispose();
+    }
+}
